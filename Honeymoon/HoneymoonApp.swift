e0fbc9d64d2c4532f49c1dd42b507cf86d3adeba
Ryan Smetana <ryan.smetana@xavware.com>
2022-11-25 16:46:09 -0500

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Smetana, Ryan on 11/25/22.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
