02a5228387958371a9251deb12d09f17de612fc6
Ryan Smetana <ryan.smetana@xavware.com>
2022-11-27 16:16:59 -0500

    Initial Commit

---
+++ Honeymoon/Models/HoneymoonModel.swift
@@ -0,0 +1,14 @@
+//
+//  HoneymoonModel.swift
+//  Honeymoon
+//
+//  Created by Smetana, Ryan on 11/26/22.
+//
+
+import SwiftUI
+
+struct Destination {
+    var place: String
+    var country: String
+    var image: String
+}
---
+++ Honeymoon/View/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Honeymoon
+//
+//  Created by Smetana, Ryan on 11/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    //MARK: - PROPERTIES
+    @State var showAlert: Bool = false
+    
+    var body: some View {
+        VStack {
+            HeaderView()
+            Spacer()
+            CardView(honeymoon: honeymoonData[2])
+                .padding()
+            Spacer()
+            FooterView(showBookingAlert: $showAlert)
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(
+                title: Text("SUCCESS"),
+                message: Text("Wishing a lovely and most precious of the times together for the amazing couple."),
+                dismissButton: .default(Text("Happy Honeymoon!"))
+            )
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
