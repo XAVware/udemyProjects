9ab603c075f87caaddf3fa4f0cad73def536142a
Ryan Smetana <ryan.smetana@xavware.com>
2022-11-30 18:55:18 -0500

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Smetana, Ryan on 11/30/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
