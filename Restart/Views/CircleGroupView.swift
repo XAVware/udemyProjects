b31875e38a36a7282ffbd39bc732a231a0182381
Ryan Smetana <ryan.smetana@xavware.com>
2022-12-02 18:10:21 -0500

    Build home screen and make the character/ring reusable

---
+++ Restart/Views/CircleGroupView.swift
@@ -0,0 +1,37 @@
+//
+//  CircleGroupView.swift
+//  Restart
+//
+//  Created by Smetana, Ryan on 12/2/22.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    // MARK: - Properties
+    @State var shapeColor: Color
+    @State var shapeOpacity: Double
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(shapeColor.opacity(shapeOpacity), lineWidth: 50)
+                .frame(width: 260, height: 260, alignment: .center)
+            
+            Circle()
+                .stroke(shapeColor.opacity(shapeOpacity), lineWidth: 80)
+                .frame(width: 260, height: 260, alignment: .center)
+        }
+    }
+}
+
+struct CircleGroupView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            CircleGroupView(shapeColor: .white, shapeOpacity: 0.2)
+        }
+            
+    }
+}
