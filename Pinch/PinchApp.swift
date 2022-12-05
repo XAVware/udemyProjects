8746bcd028e1e15d8bb131c1e2f98ca2814688b5
Ryan Smetana <ryan.smetana@xavware.com>
2022-12-04 23:57:20 -0500

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Smetana, Ryan on 12/4/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
