84182bf7754eb52115c22e130200245c0cd2f6e2
Ryan Smetana <ryan.smetana@xavware.com>
2022-12-05 13:51:02 -0500

    Initial Commit

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Smetana, Ryan on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
