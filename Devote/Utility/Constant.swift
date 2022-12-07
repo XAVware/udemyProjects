783eaa83fc3afe52985ab4861433a253e2c410b1
Ryan Smetana <ryan.smetana@xavware.com>
2022-12-06 19:53:38 -0500

    Organize code

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Ryan Smetana on 12/6/22.
+//
+
+import SwiftUI
+
+
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+
+// MARK: - UX
