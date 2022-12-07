d6155bb104bcb9bc1e6fe4a9c46227622abed04a
Ryan Smetana <ryan.smetana@xavware.com>
2022-12-07 13:57:21 -0500

    Add blur effect to background of new task view

--- Devote/Views/ContentView.swift
+++ Devote/Views/ContentView.swift
@@ -110,10 +110,15 @@ struct ContentView: View {
                     .padding(.vertical, 0)
                     .frame(maxWidth: 640)
                 } //: VStack
+                .blur(radius: showNewTaskItem ? 8.0 : 0, opaque: false)
+                .transition(.move(edge: .bottom))
+                .animation(.easeOut(duration: 0.5), value: showNewTaskItem)
                 
                 // MARK: - New Task Item
                 if showNewTaskItem {
-                    BlankView()
+                    BlankView(
+                        backgroundColor: isDarkMode ? Color.black : Color.gray,
+                        backgroundOpacity: isDarkMode ? 0.3 : 0.5)
                         .onTapGesture {
                             withAnimation() {
                                 showNewTaskItem = false
@@ -132,6 +137,7 @@ struct ContentView: View {
             } //: Toolbar
             .background(
                 BackgroundImageView()
+                    .blur(radius: showNewTaskItem ? 8.0 : 0, opaque: false)
             )
             .background(
                 backgroundGradient.ignoresSafeArea(.all)
