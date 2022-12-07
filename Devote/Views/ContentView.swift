4dd8fc8b4bfe0911bd7ba04777dd2917323e0c56
Ryan Smetana <ryan.smetana@xavware.com>
2022-12-07 14:14:12 -0500

    Add sound and haptic feedback

--- Devote/Views/ContentView.swift
+++ Devote/Views/ContentView.swift
@@ -66,6 +66,8 @@ struct ContentView: View {
                         //Appearance
                         Button {
                             isDarkMode.toggle()
+                            playSound(sound: "sound-tap", type: "mp3")
+                            feedback.notificationOccurred(.success)
                         } label: {
                             Image(systemName: isDarkMode ? "moon.circle.fill" : "moon.circle")
                                 .resizable()
@@ -81,6 +83,8 @@ struct ContentView: View {
                     // MARK: - New Task Button
                     Button {
                         showNewTaskItem = true
+                        playSound(sound: "sound-ding", type: "mp3")
+                        feedback.notificationOccurred(.success)
                     } label: {
                         Image(systemName: "plus.circle")
                             .font(.system(size: 30, weight: .semibold, design: .rounded))
