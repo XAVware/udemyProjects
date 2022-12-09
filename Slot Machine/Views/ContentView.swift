ef908edaa16806c6f46b927475dd4529a4295c29
Ryan Smetana <ryan.smetana@xavware.com>
2022-12-09 12:17:33 -0500

    Begin building main view

--- Slot Machine/Views/ContentView.swift
+++ Slot Machine/Views/ContentView.swift
@@ -8,14 +8,82 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - Properties
+    
+    
+    // MARK: - Body
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack {
+            // MARK: - Background
+            LinearGradient(gradient: Gradient(colors: [Color("ColorPink"), Color("ColorPurple")]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            
+            // MARK: - Interface
+            VStack(alignment: .center, spacing: 5) {
+                // MARK: - Header
+                LogoView()
+                
+                Spacer()
+                
+                
+                
+                // MARK: - Score
+                HStack {
+                    HStack {
+                        Text("Your\nCoins".uppercased())
+                            .scoreLabelStyle()
+                            .multilineTextAlignment(.trailing)
+                        
+                        Text("100")
+                            .scoreNumberStyle()
+                            .modifier(ScoreNumberModifier())
+                    }
+                    .modifier(ScoreContainerModifier())
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Text("200")
+                            .scoreNumberStyle()
+                            .modifier(ScoreNumberModifier())
+                        
+                        Text("High\nScore".uppercased())
+                            .scoreLabelStyle()
+                            .multilineTextAlignment(.leading)
+                    }
+                    .modifier(ScoreContainerModifier())
+                }
+                
+                // MARK: - Slot Machine
+                
+                // MARK: - Footer
+                Spacer()
+            } //: VStack
+            // MARK: - Buttons
+            .overlay(
+                //Reset
+                Button(action: {
+                    print("Reset")
+                }) {
+                    Image(systemName: "arrow.2.circlepath.circle")
+                }
+                .modifier(ButtonModifier()),
+                alignment: .topLeading
+            )
+            .overlay(
+                //Info
+                Button(action: {
+                    print("Info")
+                }) {
+                    Image(systemName: "info.circle")
+                }
+                .modifier(ButtonModifier()),
+                alignment: .topTrailing
+            )
+            .padding()
+            .frame(maxWidth: 720)
+            
+            // MARK: - Popup
+        } //: ZStack
     }
 }
 
