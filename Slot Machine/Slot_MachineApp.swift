996815cb2eb3a71de0f5faca24777635e41aa86a
Ryan Smetana <ryan.smetana@xavware.com>
2022-12-09 11:09:29 -0500

    Initial Commit

---
+++ Slot Machine/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot Machine
+//
+//  Created by Smetana, Ryan on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
