45440ee9ab6806af462e4ccb9e3cf6d692480d05
Ryan Smetana <ryan.smetana@xavware.com>
2022-12-12 20:19:35 -0500

    commit color constants, plist, and footer view

---
+++ Touchdown/Views/Home/FooterView.swift
@@ -0,0 +1,40 @@
+//
+//  FooterView.swift
+//  Touchdown
+//
+//  Created by Smetana, Ryan on 12/12/22.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    var body: some View {
+        VStack(alignment: .center, spacing: 10) {
+            Text("We offer the most cutting edge, comfortable, lightweight and durable football helmets in the market at affordable prices")
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(2)
+            
+            
+            Image("logo-lineal")
+                .renderingMode(.template)
+                .foregroundColor(.gray)
+            
+            Text("Copyright © Ryan Smetana\nAll Rights Reserved")
+                .font(.footnote)
+                .fontWeight(.bold)
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(1)
+        }
+        .padding()
+    }
+}
+
+struct FooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FooterView()
+            .previewLayout(.sizeThatFits)
+            .background(colorBackground)
+    }
+}
