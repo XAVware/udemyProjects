3f648b1ed83c7e65eda5658cd99e374f1cd09638
Ryan Smetana <ryan.smetana@xavware.com>
2022-12-12 20:00:23 -0500

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Smetana, Ryan on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
