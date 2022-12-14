a2e79bf1679de98b591c4a2bd5a67a571d397c90
Ryan Smetana <ryan.smetana@xavware.com>
2022-12-14 16:14:00 -0500

    Create shop class

---
+++ Touchdown/Utility/Shop.swift
@@ -0,0 +1,13 @@
+//
+//  Shop.swift
+//  Touchdown
+//
+//  Created by Smetana, Ryan on 12/14/22.
+//
+
+import Foundation
+
+class Shop: ObservableObject {
+    var showingProduct: Bool = false
+    var selectedProduct: Product? = nil
+}
