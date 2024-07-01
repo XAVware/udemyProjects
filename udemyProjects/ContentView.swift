07668314e7ecc4a608ac082a14e74021d2a06d2e
Ryan Smetana <ryan.smetana@xavware.com>
2024-07-01 18:01:28 -0400

    Initial Commit

---
+++ udemyProjects/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  udemyProjects
+//
+//  Created by Ryan Smetana on 7/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ udemyProjects/udemyProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  udemyProjectsApp.swift
+//  udemyProjects
+//
+//  Created by Ryan Smetana on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct udemyProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
